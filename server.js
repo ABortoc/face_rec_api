da320390ee117f9e7c31c8a198b7f92355b7b7e1
ABortoc <abortoc@gmail.com>
2020-10-17 17:26:14 -0400

    breaking up code into modules

--- server.js
+++ server.js
@@ -1,117 +1,41 @@
 const express = require('express')
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: '',
+    database: 'face-rec'
+  }
+})
 
 const app = express()
-
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      password: 'cookies',
-      email: 'john@gmail.com',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      password: 'bananas',
-      email: 'sally@gmail.com',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send(database.users)
+  res.send('Success')
 })
 
-app.post('/signin', (req, res) => {
-  // Load hash from your password DB.
-  // bcrypt.compare('apples', '$2a$10$UUJdmLSq5bdvErNpD9l5M.6Idj3rQcq6DBSN8Uvqzp05XNOFyeF.O', function (err, res) {
-  //   // res == true
-  //   if (err) {
-  //     console.log(err)
-  //   }
-  //   console.log('first guess', res)
-  // })
-  // bcrypt.compare('veggies', '$2a$10$UUJdmLSq5bdvErNpD9l5M.6Idj3rQcq6DBSN8Uvqzp05XNOFyeF.O', function (err, res) {
-  //   // res = false
-  //   if (err) {
-  //     console.log(err)
-  //   }
-  //   console.log('second guess', res)
-  // })
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-    res.json(database.users[0])
-  } else {
-    res.status(400).json('error logging in')
-  }
-  res.json('signin')
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body
-  bcrypt.hash(password, null, null, function (err, hash) {
-    // Store hash in your password DB.
-    if (err) {
-      console.log(err)
-    }
-    console.log(hash)
-  })
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length - 1])
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      return res.json(user)
-    }
-  })
-  if (!found) {
-    res.status(400).json('Not found')
-  }
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.put('/image', (req, res) => {
-  const { id } = req.body
-  let found = false
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true
-      user.entries++
-      return res.json(user.entries)
-    }
-  })
-  if (!found) {
-    res.status(400).json('Not found')
-  }
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) })
 
 app.listen(3001, () => {
-  console.log('app is running on port 3001')
+  console.log('app is running')
 })
